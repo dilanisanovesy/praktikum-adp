fd3474922adcd9f380cb484cbddad28b8255afa1
2310432012_dilanisanovesy <162921955+dilanisanovesy@users.noreply.github.com>
2024-05-29 04:03:57 +0700

    Add files via upload

---
+++ Modul9.2 Dilanisa Novesy Triananda.py
@@ -0,0 +1,62 @@
+import os
+os.system('cls')
+nama = "Dilanisa Novesy Triananda"
+nim = 2310432012
+shift = "1"
+print("Nama     : " + nama)
+print("NIM      :",nim)
+print("Shift    : " + shift)
+print()
+import os
+from termcolor import colored, cprint
+
+cprint("Program Membuat Rumah Kreatif", "white", "on_light_red")
+print()
+
+for i in range(1):
+    print(" "*40, end=" ")
+    cprint(" "*50, "dark_grey", "on_dark_grey")
+    print(" "*35, end=" ")
+    cprint(" "*60, "dark_grey", "on_dark_grey")
+    print(" "*30, end=" ")
+    cprint(" "*70, "dark_grey", "on_dark_grey")
+    print(" "*25, end=" ")
+    cprint(" "*80, "dark_grey", "on_dark_grey")
+
+for j in range(5):
+      print(" "*40, end=" ")
+      cprint(" "*6, "light_grey", "on_light_grey", end=" ")
+      cprint(" "*5, "yellow", "on_yellow", end=" ")
+      cprint(" "*25, "light_grey", "on_light_grey", end=" ")
+      cprint(" "*5, "yellow", "on_yellow", end=" ")
+      cprint(" "*5, "light_grey", "on_light_grey", end=" ")
+      print()
+
+for k in range(5):
+      print(" "*40, end=" ")
+      cprint(" "*50, "light_grey", "on_light_grey", end=" ")
+      print()
+
+for l in range(1):
+    print(" "*30, end=" ")
+    cprint(" "*70, "dark_grey", "on_dark_grey")
+    print(" "*25, end=" ")
+    cprint(" "*80, "dark_grey", "on_dark_grey")
+    print(" "*20, end=" ")
+    cprint(" "*90, "dark_grey", "on_dark_grey")
+    print(" "*15, end=" ")
+    cprint(" "*100, "dark_grey", "on_dark_grey")
+
+for m in range(7):
+      print(" "*32, end=" ")
+      cprint(" "*66, "light_grey", "on_light_grey", end=" ")
+      print()
+
+for n in range(7):
+      print(" "*32, end=" ")    
+      cprint(" "*22, "light_grey", "on_light_grey", end=" ")
+      cprint(" "*20, "cyan", "on_cyan", end=" ")
+      cprint(" "*22, "light_grey", "on_light_grey", end=" ")
+      print()
+
+print()
